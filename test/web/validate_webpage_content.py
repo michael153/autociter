4998b51c1b0ab9f05149a94ba4cc7fe7e5198c7a
Balaji Veeramani <bveeramani@berkeley.edu>
2018-11-11 21:59:56 -0800

    Add title recognition accuracy test

--- test/web/validate_webpage_content.py
+++ test/web/validate_webpage_content.py
@@ -14,37 +14,24 @@
 #
 # Author: Balaji Veeramani <bveeramani@berkeley.edu>
 """Define functions for verifying the accuracy of content extractors."""
+from difflib import SequenceMatcher
+
 import assets
 
 from autociter.data.storage import Table
+from autociter.data.standardization import std_text
 from autociter.utils.statistics import average
 from autociter.web.webpages import Webpage
 from autociter.core.pipeline import slice_text
 
 SAMPLE_DATA = Table(assets.MOCK_DATA_PATH + "/citations_sample.csv")
 IGNORED_FIELDS = {"url", "date"}
-RELEVANT_FIELDS = [
+CONSIDERED_FIELDS = [
     field for field in SAMPLE_DATA.fields if field not in IGNORED_FIELDS
 ]
-DESIRED_ACCURACY = 0.95
 
 
-def validate_accuracy_of_webpage_content(sample):
-    """Check if webpage content is sufficiently accuracy against a sample.
-
-    Arguments:
-        sample: A Table instance of sample data
-    """
-    average_accuracy = average_accuracy_of_webpage_content(sample)
-    if average_accuracy < DESIRED_ACCURACY:
-        print("Expected more than {0} accuracy but got {1}.".format(
-            DESIRED_ACCURACY, average_accuracy))
-    else:
-        print(
-            "Validation succeeded with {0} accuracy.".format(average_accuracy))
-
-
-def average_accuracy_of_webpage_content(sample):
+def data_preservation_accuracy(sample):
     """Return the average correctness of webpage content against a sample.
 
     Accuracy is defined as the number of values found in the the content
@@ -55,38 +42,62 @@ def average_accuracy_of_webpage_content(sample):
     Arguments:
         sample: A Table instance of sample data
     """
-
-    def standardize(text):
-        return text.lower()
-
     accuracies = []
     for record in sample:
         webpage = Webpage(record["url"])
         defined_values = {
-            record[field] for field in RELEVANT_FIELDS if record[field]
+            record[field] for field in CONSIDERED_FIELDS if record[field]
         }
         values_in_source = {
             value for value in defined_values
-            if value in standardize(webpage.source)
+            if value in std_text(webpage.source)
         }
         expected_values = defined_values.intersection(values_in_source)
         values_in_content = {
             value for value in expected_values
-            if value in standardize(slice_text(webpage.content))
+            if value in std_text(slice_text(webpage.content))
         }
         num_values_expected = len(expected_values)
         num_values_found = len(values_in_content)
-        differences = expected_values - values_in_content
-        if differences:
-            print("[{0}, {1}] {2}".format(num_values_found, num_values_expected,
-                                          record["url"]))
-            print("Diff: {0}".format(differences))
-        else:
-            print("ok...{0}".format(record["url"]))
-        accuracy = num_values_found / num_values_expected
-        accuracies.append(accuracy)
+        accuracies.append(num_values_found / num_values_expected)
     return average(accuracies)
 
 
-if __name__ == "__main__":
-    validate_accuracy_of_webpage_content(SAMPLE_DATA)
+def title_recognition_accuracy(sample):
+    """Return average accuracy at identifying webpage titles.
+
+    This function compares predicted webpage titles, as they are defined by
+    Webpage.content, against the actual webpage titles.
+
+    Arguments:
+        sample: A Table instance of sample data
+    """
+
+    def similarity(string1, string2):
+        """Return a score representing the similartiy of two strings."""
+        return SequenceMatcher(None, string1, string2).ratio()
+
+    def retrieve_title_from_content(content):
+        """Retrieve the predicted title from a webpage's content.
+
+        This function assumes that the title is in a heading at the top of
+        content, and that content is formatted in markdown.
+
+        Arguments:
+            content: A string returned by a webpage's content property
+
+        >>> retrieve_title_from_content("# Foo Bar \nBaz")
+        'Foo Bar'
+        """
+        whitespace_index = content.find(" ")
+        newline_index = content.find("\n", whitespace_index)
+        return content[whitespace_index + 1:newline_index].rstrip()
+
+    accuracies = []
+    for record in sample:
+        webpage = Webpage(record["url"])
+        expected_title = record["title"]
+        predicted_title = retrieve_title_from_content(webpage.content)
+        print(similarity(expected_title, predicted_title), "\n", expected_title, "\n", predicted_title)
+        accuracies.append(similarity(expected_title, predicted_title))
+    return average(accuracies)
