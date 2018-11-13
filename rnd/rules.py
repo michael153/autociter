382fb9bc60bccbb1c5c304eec96fc37dfac7f6d4
Balaji Veeramani <bveeramani@berkeley.edu>
2018-11-13 03:48:35 -0800

    Fix Rule object

--- rnd/rules.py
+++ rnd/rules.py
@@ -42,7 +42,7 @@ class Rule:
         while self.left in string and self.right in string:
             left_start = string.find(self.left)
             left_end = left_start + len(self.left)
-            right_start = string.find(self.right)
+            right_start = string.find(self.right, left_end)
             right_end = right_start + len(self.right)
             values.add(string[left_end:right_start])
             string = string[right_end:]
