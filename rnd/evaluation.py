56ce4ca09577a4c5b63b767bda17f673d35a9764
Balaji Veeramani <bveeramani@berkeley.edu>
2018-11-15 14:13:03 -0800

    Improve evaluation.py debugging output

--- rnd/evaluation.py
+++ rnd/evaluation.py
@@ -15,25 +15,29 @@
 # Author: Balaji Veeramani <bveeramani@berkeley.edu>
 import time
 
+from autociter.utils.debugging import debug
 
-def evaluate(rules, titles, num_rules=200):
+
+def evaluate(rules, titles):
     rules = remove_duplicates(rules)
     for rule in rules:
         for source_code in titles:
             rule.train(source_code, titles[source_code])
     rules = sorted(rules, key=lambda rule: -rule.weight)
 
-    print("Evaluated", len(rules), "rules")
-    print("---")
-    for rule in rules[:num_rules]:
-	       print(rule.weight, rule, sep="\t")
-    print("")
+    debug("Rule evaluation complete")
+    debug("")
+    debug("SUMMARY")
+    debug("---")
+    for rule in rules[:20]:
+	       debug(rule.weight, rule, sep="\t")
+    debug("")
 
-    return rules[:num_rules]
+    return rules
 
 
 def remove_duplicates(rules):
-
+    
     def same(rule1, rule2):
         return rule1.left == rule2.left and rule1.right == rule2.right
 
@@ -43,7 +47,8 @@ def remove_duplicates(rules):
         for j in range(i + 1, len(rules)):
             if same(rules[i], rules[j]):
                 to_remove.append(rules[j])
-        for rule in to_remove:
-            rules.remove(rule)
+        while to_remove:
+            duplicate_rule = to_remove.pop()
+            rules.remove(duplicate_rule)
         i += 1
     return rules
