dcc8bf7299dbd31d6767fd4e8ef96b8a1b5e9a5a
Balaji Veeramani <bveeramani@berkeley.edu>
2018-11-15 14:24:34 -0800

    Update rule data (#30)
    **Additions**
    * Added `debugging` module, which contains the `debug` function
    
    **Changes**
    * Added boilerplates `creation, evaluation, and execution`
    * Optimized duplicate rule removal in `remove_duplicates`
    * Change debugging output in `evaluate`

--- rnd/evaluation.py
+++ rnd/evaluation.py
@@ -1,20 +1,37 @@
+# Copyright 2018 Balaji Veeramani, Michael Wan
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# Author: Balaji Veeramani <bveeramani@berkeley.edu>
 import time
 
+from autociter.utils.debugging import debug
 
-def evaluate(rules, titles, num_rules=200):
+
+def evaluate(rules, titles):
     rules = remove_duplicates(rules)
     for rule in rules:
         for source_code in titles:
             rule.train(source_code, titles[source_code])
     rules = sorted(rules, key=lambda rule: -rule.weight)
 
-    print("Evaluated", len(rules), "rules")
-    print("---")
-    for rule in rules[:num_rules]:
-	       print(rule.weight, rule, sep="\t")
-    print("")
-
-    return rules[:num_rules]
+    debug("Rule evaluation complete")
+    debug("")
+    debug("SUMMARY")
+    debug("---")
+    for rule in rules[:20]:
+	       debug(rule.weight, rule, sep="\t")
+    return rules
 
 
 def remove_duplicates(rules):
@@ -28,7 +45,8 @@ def remove_duplicates(rules):
         for j in range(i + 1, len(rules)):
             if same(rules[i], rules[j]):
                 to_remove.append(rules[j])
-        for rule in to_remove:
-            rules.remove(rule)
+        while to_remove:
+            duplicate_rule = to_remove.pop()
+            rules.remove(duplicate_rule)
         i += 1
     return rules
