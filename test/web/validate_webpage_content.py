ed7e52a8f323742963a98f4eb11e9ced24df8ffc
Michael Wan <m.wan@berkeley.edu>
2018-11-29 05:10:30 -0800

    Update data preservation accuracy tester method

--- test/web/validate_webpage_content.py
+++ test/web/validate_webpage_content.py
@@ -17,18 +17,20 @@
 from difflib import SequenceMatcher
 
 import numpy
+import time
 
 import assets
 
 from autociter.core.pipeline import slice_text
-from autociter.data.standardization import standardize
 from autociter.data.storage import Table
 from autociter.web.webpages import Webpage
-
 from autociter.utils.debugging import debug
 
-SAMPLE_DATA = Table(assets.MOCK_DATA_PATH + "/citations_sample.csv")
-IGNORED_FIELDS = {"url", "date"}
+import autociter.data.standardization as standardization
+
+SAMPLE_DATA = standardization.standardize(
+    Table(assets.MOCK_DATA_PATH + "/citations_sample.csv"), 'table')
+IGNORED_FIELDS = {"url", "publisher"}
 CONSIDERED_FIELDS = [
     field for field in SAMPLE_DATA.fields if field not in IGNORED_FIELDS
 ]
@@ -61,34 +63,63 @@ def data_preservation_accuracy(sample):
     """
     debug("Starting data preservation test.")
     accuracies = []
+    overall = {x: {"found": 0, "total": 0} for x in CONSIDERED_FIELDS}
+
+    no_title_found = []
+
     for record in sample:
         try:
-            defined_values = {
-                record[field] for field in CONSIDERED_FIELDS if record[field]
-            }
+            if ".pdf" in record["url"]:
+                continue
+            start_time = time.time()
+            defined_values = [(record[field], field)
+                              for field in CONSIDERED_FIELDS
+                              if (record[field])]
             content = Webpage(record["url"]).content
-            standardized_content = slice_text(standardize(content, "text"))
-            values_in_content = {
-                value for value in defined_values
-                if value in standardized_content
-            }
-            num_values_expected = len(defined_values)
-            num_values_found = len(values_in_content)
-            accuracy = num_values_found / num_values_expected if num_values_found else 0
-            debug("({0} : {1}) | {2}".format(
-                num_values_found, num_values_expected, record["url"]))
+            standardized_content = slice_text(
+                standardization.standardize(content, "text"))
+            values_found_in_content = 0
+            values_expected_in_content = 0
+            for value, field in defined_values:
+                location = standardization.find(value, standardized_content,
+                                                field)
+                if isinstance(location, list):
+                    for loc in location:
+                        values_expected_in_content += 1
+                        overall[field]['total'] += 1
+                        if loc != (-1, -1):
+                            overall[field]['found'] += 1
+                            values_found_in_content += 1
+                else:
+                    values_expected_in_content += 1
+                    overall[field]['total'] += 1
+                    if location != (-1, -1):
+                        overall[field]['found'] += 1
+                        values_found_in_content += 1
+                    elif field == 'title':
+                        no_title_found.append(record["url"])
+
+            accuracy = values_found_in_content / values_expected_in_content if values_found_in_content else 0
+            debug("({0} : {1}) | {2} | {3:.2f}s".format(
+                values_found_in_content, values_expected_in_content,
+                record["url"],
+                time.time() - start_time))
             accuracies.append(accuracy)
         except Exception as ex:  #pylint: disable=broad-except
             debug("Error   | {0} | {1}".format(record["url"], ex))
     average_accuracy = numpy.average(accuracies)
-
     debug("Data accuracy test complete.\n")
     debug("SUMMARY")
     debug("---")
-    debug("average accuracy:", average_accuracy)
-    debug("# records tested:", len(sample))
+    debug("Average Accuracy:", average_accuracy)
+    debug("# Records Tested:", len(sample))
     debug("")
-
+    for field in overall:
+        debug("Field {0}: ({1} / {2} found)".format(
+            field, overall[field]['found'], overall[field]['total']))
+    debug("\n")
+    debug("\n\nUrls w/ no title found: ")
+    debug(no_title_found)
     return average_accuracy
 
 
@@ -115,8 +146,7 @@ def content_start_accuracy(sample):
         expected_title = record["title"]
         try:
             predicted_title = retrieve_title_from_content(
-                standardize(webpage.content, "text"),
-                len(expected_title))
+                standardize(webpage.content, "text"), len(expected_title))
             sim = similarity(expected_title.title(), predicted_title.title())
             debug(
                 record["url"],
