90972d0ad8d1d17ec0bf290e3954e30dd3f4c46f
Balaji Veeramani <bveeramani@berkeley.edu>
2018-12-17 08:27:46 -0600

    Implement non-functional CLI

--- autociter/__init__.py
+++ autociter/__init__.py
@@ -13,3 +13,56 @@
 #   limitations under the License.
 #
 # Author: Balaji Veeramani <bveeramani@berkeley.edu>
+"""Implements command-line interface and high-level functionality."""
+import argparse
+import sys
+
+from autociter.core.errors import AutociterError
+
+__version__ = '0.0.0'
+
+
+def main(argv):
+    """Main program.
+
+  Arguments:
+      argv: command-line arguments, such as sys.argv (including the program name
+            in argv[0]).
+
+  Returns:
+      Zero on successful program termination, non-zero otherwise.
+  """
+    parser = argparse.ArgumentParser(description='Automated citation tool.')
+    parser.add_argument(
+        '-v',
+        '--version',
+        action='store_true',
+        help='show version number and exit')
+    parser.add_argument(
+        '--style', action='store', help='specify formatting style')
+    parser.add_argument(
+        '-vv',
+        '--verbose',
+        action='store_true',
+        help='display all extracted data')
+    parser.add_argument('urls', nargs='*', help='specify which URLs to cite.')
+    args = parser.parse_args(argv[1:])
+
+    if args.version:
+        print('autociter {}'.format(__version__))
+        return 0
+
+    return 0
+
+
+def run_main():
+    """Run main method and return appropriate exit code."""
+    try:
+        sys.exit(main(sys.argv))
+    except AutociterError as e:  #pylint: disable=invalid-name
+        sys.stderr.write('autociter: ' + str(e) + '\n')
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    run_main()
